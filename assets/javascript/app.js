85bd5cc6da9c2692868d966492285deb82941144
Angie Roach <angieroach4@gmail.com>
2019-09-22 17:56:21 -0400

    styled images to display inline

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 
 
 // initial array of topics
-var topics = ["Mandelbrot", "Timelapse Stars", "Timelapse Flowers", "Timelapse Lightning-storm", "Sunset", "Volcano", "Sea", "Dancing Bird", "Groot"];
+var topics = ["Mandelbrot", "Timelapse Stars", "Timelapse Flowers", "Timelapse Lightning-storm", "Smile", "Sunset", "Volcano", "Sea", "Dancing Bird", "Groot"];
 var button;
 
 function createButtons() {                                              // to display gif data
@@ -25,7 +25,7 @@ function createButtons() {                                              // to di
     // This function handles events where a movie button is clicked
     $("#add-gif").on("click", function (event) {
         event.preventDefault();
-        
+
         var gif = $("#gif-input").val().trim();                           // This line grabs the input from the textbox
         topics.push(gif);                                                 // Adding gifs from the textbox to our array                        
         createButtons();                                                  // Call function to render buttons
@@ -49,32 +49,35 @@ function getGIFs() {                                                      // dis
         var response = response.data;                                     // array of results returned 
 
         for (var i = 0; i < response.length; i++) {                       // loop results of the array for individual topics
-            var giphyDiv = $("<div>");                                    // container for the individual gifs
+            var giphyDiv = $("#gifsHere");                                // container for the individual gifs
+            var gifHolder = $("<div class='inline'>");                                 
+            var gifImg = $("<img class='animatedGIF'>");                         // image tag
+            gifImg.attr("data-state", "still"); 
+            console.log(gifImg);                                   
+            gifImg.attr("src", response[i].images.fixed_height_still.url);    //src attribute of results data properties
+            gifImg.attr("data-still", response[i].images.fixed_height_still.url);
+            gifImg.attr("data-animate", response[i].images.fixed_height_still.url);
+            
             var rating = response[i].rating;                              // ratings to display for the gifs
             var ratingCopy = $("<p>").text("Rating: " + rating);          // p tag for ratings display on html
-            var giphyImg = $("<img>");                                    // image tag
-            giphyImg.attr("src", response[i].images.fixed_height_still.url);    //src attribute of results data properties
-            giphyImg.attr("data-still", response[i].images.fixed_height_still.url);
-            giphyImg.attr("data-animate", response[i].images.fixed_height_still.url);
-            giphyImg.attr("data-state", "still");
-            giphyDiv.append(ratingCopy);                                  // adds the rating and image to the giphyDiv
-            giphyDiv.append(giphyImg);
-            $("#gifsHere").prepend(giphyDiv);                             // 'prints giphy to html
+            
+            gifHolder.append(ratingCopy);                                  // adds the rating and image to the giphyDiv
+            gifHolder.append(gifImg);
+            $("#gifsHere").prepend(gifHolder);                             // 'prints giphy to html
         }
+        $(".animatedGIF").on("click", function () {                             //setting up the still/animate/still function
+            
+            var state = $(this).attr("data-state");
+    
+            if (state === "still") {                                             // If the image's state is still, update its src attribute to what its data-animate value is. 
+                $(this).attr("src", $(this).attr("data-animate"));               // Then, set the image's data-state to animate
+                $(this).attr("data-state", "animate");
+            } else {
+                $(this).attr("src", $(this).attr("data-still"));                 // Else set src to the data-still value
+                $(this).attr("data-state", "still");
+            }
+        })
     })
 
-    $("#gifsHere").on("click", function (event) {                             //setting up the still/animate/still function
-        event.preventDefault();
-
-        var state = $(this).attr("data-state");
-
-        if (state === "still") {                                             // If the image's state is still, update its src attribute to what its data-animate value is. 
-            $(this).attr("src", $(this).attr("data-animate"));               // Then, set the image's data-state to animate
-            $(this).attr("data-state", "animate");
-        } else {
-            $(this).attr("src", $(this).attr("data-still"));                 // Else set src to the data-still value
-            $(this).attr("data-state", "still");
-        }
-    })
 }
 
